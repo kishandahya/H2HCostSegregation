f39b15b00f9a97023dda7aa7c1dd610956961d81
Kishan Dahya <kishandahya@gmail.com>
2023-03-16 12:25:00 -0700

    Remove unnecessary files and folders 2

---
+++ _document.js
@@ -0,0 +1,24 @@
+// _document.js
+
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
